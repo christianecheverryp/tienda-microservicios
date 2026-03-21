d9b6949092b61db178a147db9d43e134002b0fd4
chris <christianecheverryp@gmail.com>
2026-03-21 19:40:10 +0100

    Primer commit: esqueleto catalogo

---
+++ src/main/java/com/tienda/catalogo/CatalogoServicioApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoServicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoServicioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tienda/catalogo/CatalogoServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoServicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
